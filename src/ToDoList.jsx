37b9cf98105205942dea4683dc612dfa8c159538
Sourav <sourav.banerjeejobs@gmail.com>
2025-09-27 21:27:16 +0530

    css & html structure done

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { useState } from "react";
 
 const ToDoList = () => {
-  return <div>ToDoList</div>;
+  const [tasks, setTasks] = useState([
+    "Wake Up",
+    "Take a Shower",
+    "Study",
+    "Eat Lunch",
+  ]);
+  const [newTask, setNewTask] = useState("");
+
+  return (
+    <div className="to-do-list">
+      <h1>To Do List</h1>
+      <div>
+        <input type="text" placeholder="Enter a task..." />
+        <button className="add-btn">Add</button>
+      </div>
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className="text">{task}</span>
+            <button className="delete-btn">Delete</button>
+            <button className="move-btn">⬆</button>
+            <button className="move-btn">⬇</button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 };
 
 export default ToDoList;
